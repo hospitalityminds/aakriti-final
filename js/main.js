cc7c5098922f8c2e7559a5dd3cd19c36ae73a754
hospitalityminds <website.hospitalityminds@gmail.com>
2021-05-05 14:38:51 +0530

    files added

--- js/main.js
+++ js/main.js
@@ -34,22 +34,22 @@
 
     //Javascript function scrolling up
 
-			// mybutton=document.getElementById("mybtn");
+			mybutton=document.getElementById("mybtn");
 
 
-			// window.onscroll = function(){ scrollFunction()};
+			window.onscroll = function(){ scrollFunction()};
 
-			// function scrollFunction(){
-			// if(document.body.scrollTop>20 || document.documentElement.scrollTop>20){
-			// 	mybutton.style.display = "block";
-			// }
-			// else{
-			// 	mybutton.style.display="none";
-			// }
-			// }
+			function scrollFunction(){
+			if(document.body.scrollTop>20 || document.documentElement.scrollTop>20){
+				mybutton.style.display = "block";
+			}
+			else{
+				mybutton.style.display="none";
+			}
+			}
 
 
-			// function topfunction(){
-			// document.body.scrollTop=0;   
-			// document.documentElement.scrollTop=0;      
-			// }
+			function topfunction(){
+			document.body.scrollTop=0;   
+			document.documentElement.scrollTop=0;      
+			}
