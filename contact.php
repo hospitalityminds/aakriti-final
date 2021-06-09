390f0ea9f823226110f59317d971e969107d51e7
hospitalityminds <website.hospitalityminds@gmail.com>
2021-06-09 17:26:54 +0530

    files added

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
 		$phone=$_POST['phone'];
 		$msg=$_POST['msg'];
 
-		$to='amar@linkpromotions.co.in'; // Receiver Email ID, Replace with your email ID
+		$to='amar@aakritispacedesigns.com'; // Receiver Email ID, Replace with your email ID
 		$subject='Form Submission';
 		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
 		$headers="From: ".$email;
